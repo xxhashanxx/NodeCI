c177ff64a7f5cf26429dccdac2f5872111877f1b
malka liyanage <malka.liyanage96@gmail.com>
2021-02-23 11:10:27 +0530

    change

--- tests/header.test.js
+++ tests/header.test.js
@@ -25,7 +25,7 @@ test('clicking login starts oauth flow', async () => {
   expect(url).toMatch(/accounts\.google\.com/);
 });
 
-test.only('When signed in, shows logout button', async () => {
+test('When signed in, shows logout button', async () => {
   await page.login();
 
   const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
--- tests/setup.js
+++ tests/setup.js
@@ -1,4 +1,4 @@
-jest.setTimeout(30000);
+jest.setTimeout(10000);
 
 require('../models/User');
 
